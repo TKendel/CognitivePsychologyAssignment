e47c9c5ee4942d8f80b2181aef388805a2895a72
TKendel <kendeltoni@gmail.com>
2023-10-06 09:35:30 +0200

    feat: added json with data example and unpacking in the main for potential looping over the paragraphs

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import json
+import random
 import tkinter as tk
 from tkinter import Button
 from tkinter import INSERT, END
@@ -14,6 +17,7 @@ class Word_by_word_highlighter:
         self.TKtext.tag_config("highlight", background="red")
 
     def start_highlighting(self):
+        initial = time.time()
         words = self.text.split()
         for word in words:
             self.start = self.TKtext.search(word, self.start, nocase=1, stopindex=END)
@@ -23,6 +27,9 @@ class Word_by_word_highlighter:
                 self.TKtext.update()
                 self.TKtext.after(5, self.remove_highlight(end))
                 self.start = end
+        final = time.time()
+        print(final - initial)
+
 
     def add_highlight(self, end):
         self.TKtext.tag_add('highlight', self.start, end)
@@ -32,14 +39,26 @@ class Word_by_word_highlighter:
 
 
 def main():
+    # Opening JSON file
+    f = open('assignment\qna.json')
+
+    # Reading from file
+    data = json.loads(f.read())
+
+    # Closing file
+    f.close()
+
+    # Get the comperhension paragraphs from the loaded data
+    questions = data["comperhnesion_paragraphs"][0]
+    # Pick a random question from the list
+    question_number, question_content = random.choice(list(questions.items()))
+
     root = tk.Tk()
     root.title("Highlighting")
 
-    text = "Is this working? Lets check it out. This is a test"
-
-    word_higlihter = Word_by_word_highlighter(root, text)
-    B = Button(root, text ="Start", command = word_higlihter.start_highlighting)
-    B.place(x=280,y=300)
+    word_higlihter = Word_by_word_highlighter(root, question_content[0]["paragraph"])
+    start = Button(root, text ="Start", command = word_higlihter.start_highlighting)
+    start.place(x=280,y=300)
     root.mainloop()
 
 if __name__ == "__main__":
